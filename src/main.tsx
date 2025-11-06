805a6295dafc35d7eb9aebc4a29d474079ddc6c4
Drew Jex <drewjex@amazon.com>
2025-11-06 09:49:21 -0700

    portfolio content

---
+++ src/main.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App4 from "./App4.tsx"
+import './index2.css'
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+
+library.add(fas, far, fab)
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App4 />
+  </React.StrictMode>,
+)
